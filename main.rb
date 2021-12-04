d6c01904f3c2720a2a53c3d22ed0799de8f9ee61
misitm <j1831@matsue-ct.jp>
2021-12-04 06:53:55 +0000

    いろいろ

--- main.rb
+++ main.rb
@@ -5,19 +5,20 @@ include DXOpal
 
 require_remote 'player.rb'
 require_remote 'enemy.rb'
+require_remote 'block.rb'
 
 Image.register(:player, 'images/sq.png') 
 Image.register(:enemy, 'images/sq.png') 
 
-Image.register(:brick, 'images/sq.png') 
+Image.register(:brick, 'images/Renga.png') 
 Image.register(:tile, 'images/sq.png') 
 Image.register(:asphalt, 'images/sq.png') 
 
-field = CSV.read("field.csv")
+#field = CSV.read("field.csv")
 
 Window.load_resources do
-  Window.width  = 800
-  Window.height = 600
+  Window.width  = 1200
+  Window.height = 720
 
   player_img = Image[:player]
   player_img.set_color_key([0, 0, 0])
@@ -25,17 +26,28 @@ Window.load_resources do
   enemy_img = Image[:enemy]
   enemy_img.set_color_key([0, 0, 0])
   
+  brick_img = Image[:brick]
+  brick_img.set_color_key([0, 0, 0])
+  
   player = Player.new(400, 500, player_img)
 
   enemies = []
   10.times do
     enemies << Enemy.new(rand(800), rand(600), enemy_img)
   end
+  
+  blocks = []
+  10.times do |i|
+    10.times do |j|
+      blocks << Block.new(i*48,j*48,brick_img)
+    end
+  end
 
   Window.loop do
     Sprite.update(enemies)
     Sprite.draw(enemies)
-
+    Sprite.draw(blocks)
+    
     player.update
     player.draw
 
