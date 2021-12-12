6c5bbe40e08c5893e486707312352b8a58422985
Kajitani27 <inatizak0727@gmail.com>
2021-12-12 04:49:41 +0000

    カメラ移動

--- main.rb
+++ main.rb
@@ -6,6 +6,7 @@ require_remote 'player.rb'
 require_remote 'enemy.rb'
 require_remote 'block.rb'
 require_remote 'stage1.rb'
+require_remote 'stage2.rb'
 
 Image.register(:player, 'images/sq.png') 
 Image.register(:enemy, 'images/enemy.png') 
@@ -65,19 +66,74 @@ Window.load_resources do
       else
       end
       
-        
+      case $field2[i][j]
+      when 1 then
+        blocks2 << Block.new(j*48,i*48,asphalt_img,1)
+      when 0 then
+        blocks2 << Block.new(j*48,i*48,tile_img,0)
+      when 2 then
+        blocks2 << Block.new(j*48,i*48,asphalt_img,2)
+      when 6 then
+        blocks2 << Block.new(j*48,i*48,woodbox_img,6)
+      else
       end
+      
     end
   end
 
+  move=[]
+  up=0
+  down=1
+  right=2
+  left=3
+  move[0]=0
+  blocks_now=blocks
   Window.loop do
     #Sprite.update(enemies)
     #Sprite.draw(enemies)
+    if player.y <= 0 #上移動
+      move[up]=1
+    elsif player.y >= Window.height - player.image.height #下移動
+      move[down]=1
+    elsif player.x >= Window.width - player.image.width #右移動
+      move[right]=1
+    elsif player.x <= 0 #左移動
+      move[left]=1
+    end
+    
+    if move[up]==1
+      player.x=Window.width/2
+      player.y=Window.height - player.image.height
+      blocks_now=blocks2
+      move[up]=0
+    elsif move[down]==1
+      player.x=Window.width/2
+      player.y=0
+      blocks_now=blocks
+      move[down]=0
+    elsif move[right]==1
+      player.x=250
+      player.y=330
+      blocks_now=blocks
+      move[right]=0
+    elsif move[left]==1
+      player.x=250
+      player.y=300
+      blocks_now=blocks
+      move[left]=0
+    end
+    
+    if blocks_now==blocks
+      Sprite.draw(blocks)
+    elsif blocks_now==blocks2
+      Sprite.draw(blocks2)
+    end
     
     dx = Input.x*2
     dy = Input.y*2
+    
     player.update(dx,0)
-    blocks.each do |x|
+    blocks_now.each do |x|
       if [6,0].include?(x.type)
         if player === x
           player.update(-dx,0)
@@ -86,7 +142,7 @@ Window.load_resources do
       end
     end
     player.update(0,dy)
-    blocks.each do |x|
+    blocks_now.each do |x|
       if [6,0].include?(x.type)
         if player === x
           player.update(0,-dy)
