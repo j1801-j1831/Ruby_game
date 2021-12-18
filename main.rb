b0e921fea94315fa7e1a2ae4c22aaea96ab3a671
misitm <j1831@matsue-ct.jp>
2021-12-18 02:22:35 +0000

    a

--- main.rb
+++ main.rb
@@ -11,7 +11,6 @@ require_remote 'stage2.rb'
 require_remote 'stage3.rb'
 require_remote 'stage4.rb'
 
-Image.register(:player, 'images/front_player1_touka.png') 
 Image.register(:player0, 'images/player_move.png') 
 Image.register(:player1, 'images/player_stop.png') 
 Image.register(:player2, 'images/player_move_flip.png') 
@@ -21,7 +20,6 @@ Image.register(:player5, 'images/player_back_flip.png')
 Image.register(:player6, 'images/front_player1_touka.png') 
 Image.register(:player7, 'images/front_player1_touka_flip.png') 
 
-Image.register(:enemy, 'images/dorobo_front_left_small.png') 
 Image.register(:enemy0, 'images/dorobo_walk_small.png') 
 Image.register(:enemy1, 'images/dorobo_stop_small.png') 
 Image.register(:enemy2, 'images/dorobo_walk_small_flip.png') 
@@ -29,19 +27,20 @@ Image.register(:enemy3, 'images/dorobo_stop_small_flip.png')
 Image.register(:enemy4, 'images/dorobo_back_left_small.png') 
 Image.register(:enemy5, 'images/dorobo_back_right_small.png') 
 Image.register(:enemy6, 'images/dorobo_front_left_small.png') 
-Image.register(:enemy7, 'images/dorobo_front_right_small.png')
+Image.register(:enemy7, 'images/dorobo_front_right_small.png') 
 
 Image.register(:brick, 'images/Renga.png') 
 Image.register(:tile, 'images/tile.png') 
 Image.register(:asphalt, 'images/asufaruto.png') 
 Image.register(:wood, 'images/wood.png') 
-Image.register(:woodbox, 'images/woodbox.png') 
+Image.register(:woodbox, 'images/woodbox_2.png') 
 
-Image.register(:bullet, 'images/MyShot.png') 
+Image.register(:bullet0, 'images/TNT_left.png') 
+Image.register(:bullet1, 'images/TNT_right.png') 
+Image.register(:bullet2, 'images/TNT_down.png')
+Image.register(:bullet3, 'images/TNT_up.png')
 
 Image.register(:sq, 'images/sq.png') 
-Image.register(:heart, 'images/HP.png')
-Image.register(:heartB, 'images/HP_black.png')
 
 Window.load_resources do
   Window.width  = 1200
@@ -50,18 +49,17 @@ Window.load_resources do
   Height = 15
   Width = 25
 
-  player_img = Image[:player]
-  player_img.set_color_key([0, 0, 0])
-  
   player_imgs = [Image[:player0],Image[:player1],Image[:player2],Image[:player3],Image[:player4],Image[:player5],Image[:player6],Image[:player7]]
   player_imgs.each do |x|
     x.set_color_key([0, 0, 0])
   end
 
-  enemy_img = Image[:enemy]
-  enemy_img.set_color_key([0, 0, 0])
-  
   enemy_imgs = [Image[:enemy0],Image[:enemy1],Image[:enemy2],Image[:enemy3],Image[:enemy4],Image[:enemy5],Image[:enemy6],Image[:enemy7]]
+  enemy_imgs.each do |x|
+    x.set_color_key([0, 0, 0])
+  end
+  
+  bullet_imgs = [Image[:bullet0],Image[:bullet1],Image[:bullet2],Image[:bullet3]]
   enemy_imgs.each do |x|
     x.set_color_key([0, 0, 0])
   end
@@ -77,17 +75,10 @@ Window.load_resources do
   woodbox_img = Image[:woodbox]
   woodbox_img.set_color_key([0, 0, 0])
   
-  bullet_img = Image[:bullet]
-  bullet_img.set_color_key([0, 0, 0])
-  
   sq_img = Image[:sq]
   sq_img.set_color_key([0, 0, 0])
-  heart_img = Image[:heart]
-  heart_img.set_color_key([0, 0, 0])
-  heartB_img = Image[:heartB]
-  heartB_img.set_color_key([0, 0, 0])
   
-  player = Player.new(240, 240, player_img, player_imgs)
+  player = Player.new(240, 240, player_imgs[0], player_imgs)
   
   fields = []
   fields << $field1
@@ -111,7 +102,7 @@ Window.load_resources do
           next
         end
         enemies_field[i][y][x]=1
-        enemies[i] << Enemy.new(x*48, y*48, enemy_img, enemy_imgs)
+        enemies[i] << Enemy.new(x*48, y*48, enemy_imgs[0], enemy_imgs)
         break
       end
     end
@@ -138,7 +129,7 @@ Window.load_resources do
   
   shots = []
 
-  move=[0,0,0,0]
+  move=[]
   up=0
   down=1
   right=2
@@ -182,23 +173,20 @@ Window.load_resources do
     
     Sprite.draw(blocks[now_stage])
     
-    hearts=Array.new(3)
-    hearts[0] = Sprite.new(0,0,heartB_img)
-    hearts[1] = Sprite.new(32,0,heartB_img)
-    hearts[2] = Sprite.new(64,0,heartB_img)
-    
-    if Input.key_push?(K_A)
-      player.decrease_hp
-    end
-    
-    player.returnhp.times do |i|
-      hearts[i] = Sprite.new(32*i,0,heart_img)
-    end
-    
-    Sprite.draw(hearts)
+    #-------------------
     
     if Input.key_push?( K_SPACE ) && shots.size < 2
-      shots << Shot.new(player.xx,player.yy,bullet_img,player.dirx*8,player.diry*8)
+      idx=0
+      if player.dirx < 0
+        idx=0
+      elsif player.dirx > 0
+        idx=1
+      elsif player.diry < 0
+        idx=2
+      elsif player.diry > 0
+        idx=3
+      end
+      shots << Shot.new(player.xx,player.yy,bullet_imgs[idx],player.dirx*8,player.diry*8)
     end
     del_shots=[]
     shots.each_with_index do |x, i|
